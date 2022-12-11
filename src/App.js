1d3d6bb319a10f1e0f02c32cbc7412d4d51d22fc
Rehan <rehanchanija@gmail.com>
2022-12-11 14:09:29 +0530

    coding start

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <h1 className='text-2xl text-red-500'>
+            Hello
+        </h1>
+        <p>
+            Hello
+        </p>
+    </div>
+  )
+}
+
+export default App
